7fe026fa5ebe063968cf22c5ece4893ad6e28ab2
antenna-three <tkduc@outlook.com>
2021-07-01 23:50:00 +0900

    Use template inheritance

--- register/views.py
+++ register/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
 
@@ -9,16 +9,14 @@ from .models import Entry
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
-
-def entry(request):
     admission_years = list(range(2021, 1975, -1))
     entry_dates = [date(2021, 9, day) for day in [10, 11, 12, 13]]
     context = {
+        'title': '参加登録',
         'admission_years': admission_years,
         'entry_dates': entry_dates,
     }
-    return render(request, 'register/entry.html', context)
+    return render(request, 'register/index.html', context)
 
 def submit(request):
     fields = ['screen_name', 'admission_year', 'start_date', 'end_date']
@@ -28,9 +26,28 @@ def submit(request):
 class ListView(generic.ListView):
     model = Entry
 
+    def get_ordering(self, **kwargs):
+        return '-admission_year'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = '登録一覧'
+        return context
+
 class DetailView(generic.DetailView):
     model = Entry
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = '登録内容'
+        context['message'] ='以下の内容で登録されています'
+        return context
+
 class ResultView(generic.DetailView):
     model = Entry
-    template_name = 'register/results.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = '登録結果'
+        context['message'] ='以下の内容で登録されました'
+        return context
